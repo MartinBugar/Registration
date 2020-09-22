57c0f046eb159bef970461951bf2c7484e8c2f6d
Martin <martin1.bugar@gmail.com>
2020-09-22 18:04:48 +0200

    jwt created

--- src/main/java/com/prometheus/Registration/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/prometheus/Registration/security/jwt/AuthEntryPointJwt.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
