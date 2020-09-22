e3fdfc6e75fcb8b52afe9f80e68b54d211e12231
Martin <martin1.bugar@gmail.com>
2020-09-22 17:01:05 +0200

    UserDetailsServiceImpl

---
+++ src/main/java/com/prometheus/Registration/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.prometheus.Registration.security.services;
+
+import com.prometheus.Registration.models.User;
+import com.prometheus.Registration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username : " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
